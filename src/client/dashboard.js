f29afe09c2258ad66292da495c6f4ebb96294e14
tomgeorge12 <tomgeorge12@gmail.com>
2019-08-06 00:43:07 +0900

    added login
    Added
    *login
    *dashboard

---
+++ src/client/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class dashboard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default dashboard;
---
+++ src/client/login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class login extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" />
+                <input type="password" />
+                <button>{"Login"}</button>
+            </div>
+        );
+    }
+}
+
+
+login.propTypes = {
+
+};
+
+export default login;
